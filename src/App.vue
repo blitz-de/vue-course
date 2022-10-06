a5ee5f243b74ea43aa5b45e28cbe214c1454ba6c
Al-absi, Sakhr <sakhr.al-absi@accenture.com>
2022-10-07 00:20:59 +0200

    using named views and sidebars

--- src/App.vue
+++ src/App.vue
@@ -16,10 +16,16 @@
       </ul>
     </nav>
   </header>
-  <main>
-      <router-view />
-      <!--Search /-->
-  </main>
+  <div class="container">
+    <aside class="aside">
+      <router-view name="sidebar" />
+    </aside>
+    <main>
+        <router-view />
+        <!--Search /-->
+    </main>
+  </div>
+
 </template>
 
 <script>
@@ -44,15 +50,14 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
 }
 main {
-  margin: 0 auto;
   padding: 30px;
   background-color: white;
-  width: 1024px;
+  width: 964px;
   min-height: 300px;
 }
 header {
   background-color: #999;
-  width: 1084px;
+  width: 1184px;
   margin: 0 auto;
 }
 ul {
@@ -75,5 +80,16 @@ ul {
 }
 .router-link-active {
   color: white;
+}
+.container {
+  display: flex;
+  margin: 10px auto 0 auto;
+  justify-content: center;
+}
+.aside {
+  padding: 30px;
+  background-color: grey;
+  width: 100px;
+  min-height: 300px;
 }
 </style>
